2590b023c9a315f9c22ffa13ad406f8efcf1f463
Jusio <Jusio@DESKTOP-TCUCO3Q>
2016-05-01 11:13:16 +0100

    ShowMyFollowingListCommand -> Incorpore funcionalidad para que detecte los usuarios con gustos similares al que esta logeado.Falta un control de errores para que no muestre los usuarios que ya hemos añadido a nuestra followingList.
    followList.jsp -> Incorpore la tabla que muestra los usuarios que tienen gustos similares al nuestro.
    
    GUSTOS SIMILARES -> Me refiero a todos aquellos usuarios que han comprado aunque sea un producto igual que nosotros.

--- mg2_5-war/src/java/controllers/ShowMyFollowingListCommand.java
+++ mg2_5-war/src/java/controllers/ShowMyFollowingListCommand.java
@@ -1,9 +1,11 @@
 package controllers;
 
 import controller.FollowerListFacade;
+import controller.SalesFacade;
 import controller.UsersFacade;
 import entities.Users;
 import entities.FollowerList;
+import entities.Sales;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,18 +26,54 @@ public class ShowMyFollowingListCommand extends FrontCommand {
             Users loggedUser = (Users) session.getAttribute("loggedUser");
             UsersFacade usersFacade = InitialContext.doLookup("java:global/mg2_5/mg2_5-ejb/UsersFacade");
             FollowerListFacade FollowerListFacade = InitialContext.doLookup("java:global/mg2_5/mg2_5-ejb/FollowerListFacade");
-            List<Users> usersList =new ArrayList<>();;
+            List<Users> usersList = new ArrayList<>();;
             List<FollowerList> followlist = FollowerListFacade.findAll();
             for (FollowerList followlistuser : followlist) {
-                if (followlistuser.getUserId() == loggedUser.getUserId()){ 
+                if (followlistuser.getUserId() == loggedUser.getUserId()) {
                     usersList.add(usersFacade.find(followlistuser.getFollower()));
                 }
             }
+
+            getMyRecomendedUsers(loggedUser);
+
             request.setAttribute("usersList", usersList);
             forward("/followList.jsp");
         } catch (ServletException | IOException | NamingException ex) {
             Logger.getLogger(ShowMyFollowingListCommand.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
+    private void getMyRecomendedUsers(Users loggedUser) throws NamingException {
+        SalesFacade saleFacade = InitialContext.doLookup("java:global/mg2_5/mg2_5-ejb/SalesFacade");
+        UsersFacade UsersFacade = InitialContext.doLookup("java:global/mg2_5/mg2_5-ejb/UsersFacade");
+        List<Sales> saleList = saleFacade.findAll();
+        List<Sales> mySaleList = new ArrayList<>();
+        List<Users> recomendedUsersList = new ArrayList<>();
+
+        for (Sales sale : saleList) {
+            if (sale.getUserId() == loggedUser.getUserId()) {
+                mySaleList.add(sale);
+            }
+        }
+
+        for (Sales sale : saleList) {
+            for (Sales mySale : mySaleList) {
+                if ((sale.getProductId() == mySale.getProductId()) && (sale.getUserId() != loggedUser.getUserId())) {
+                    if (!userInMyList(recomendedUsersList, sale.getUserId())) {
+                        recomendedUsersList.add(UsersFacade.find(sale.getUserId()));
+                    }
+                }
+            }
+        }
+        request.setAttribute("recomendedUsersList", recomendedUsersList);
+    }
+
+    private boolean userInMyList(List<Users> recomendedUsersList, int userId) {
+        for (Users user : recomendedUsersList) {
+            if (user.getUserId() == userId) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- mg2_5-war/web/followList.jsp
+++ mg2_5-war/web/followList.jsp
@@ -1,3 +1,4 @@
+<%@page import="java.util.List"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
@@ -15,20 +16,38 @@
                 <input type="hidden" name="command">
                 <input type="submit" value="Go to Main page" class="btn-link">
             </form><br><br>
-           
+
             <c:forEach var="element" items="${usersList}">
                 <b>Name: </b><br>
-               
+
                 ${element.name}<br>
-                
+
                 <br><br>
             </c:forEach><br><br>
+
+            <h1>Estos usuarios tienen gustos similares a ti</h1>
+            <table class="table-striped table table-hover">
+                <tr>
+                    <th>Name</th>
+                </tr>
+                <%                    
+                    List<Users> recomendedUsersList = (List<Users>) request.getAttribute("recomendedUsersList");
+                    for (Users user : recomendedUsersList) {
+                %>
+                <tr>
+                    <td><%=user.getName()%></td>
+                </tr>
+                <%
+                    }
+                %>
+            </table>
             
             <form action="FrontController" method="post">
                 <input type="text" name="nametofollow" placeholder="Seguir a :">
                 <input type="hidden" name="command" value="AddToFollowingListCommand">
                 <input type="submit" value="Follow">
             </form>
+        
         </div>
     </body>
 </html>
